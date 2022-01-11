b85f59b613068ed9fc7af52bdd3706e00d20e537
SanMoura <santiago.marlus@gmail.com>
2022-01-11 10:31:41 -0300

    more

--- app/Controllers/Http/AttendancesController.ts
+++ app/Controllers/Http/AttendancesController.ts
@@ -1,5 +1,8 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
+// Models
+import AttendanceModel from 'App/Models/Attendance'
+
 export default class AttendancesController {
   public async index({}: HttpContextContract) {}
 
@@ -7,7 +10,10 @@ export default class AttendancesController {
 
   public async store({}: HttpContextContract) {}
 
-  public async show({}: HttpContextContract) {}
+  public async show(id) {
+    const data = await AttendanceModel.find(id)
+    return data
+  }
 
   public async edit({}: HttpContextContract) {}
 
